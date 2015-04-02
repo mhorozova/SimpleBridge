9df4a2b97b77e8225a8284d996244a4f59331503
Milka Horozova <mhorozova@ldn.itrs>
2015-04-02 14:30:51 +0100

    sometimes writing updates to files instead of DataViews, will try to use Request Class to fix that

--- src/SimpleBridgeMain.java
+++ src/SimpleBridgeMain.java
@@ -1,5 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -29,9 +35,15 @@ public final class SimpleBridgeMain {
 			/* customer defined list of 50+ XPaths that match 20 000+ DataViews */
 			ArrayList<String> initialXPaths = new ArrayList<String>();
 			
-			// test with two XPaths
+			// test with a couple of XPaths
+			/* make sure an XPath doesn't match too many DataViews
+			 * as that may overload the node?*/
 			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-1\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
 			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-4\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-5\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//	initialXPaths.add("/geneos/gateway/directory/probe/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
 			
 			/* all XPaths 20 000+, every XPath matches a single DataView */
 			final Set<String> allXPaths = new HashSet<String>();
@@ -39,8 +51,74 @@ public final class SimpleBridgeMain {
 		/*
 		 * For each XPath, get a list of the XPaths of all matching DataViews (0 or many)
 		 */	
-		for(final String path : initialXPaths){
+		getAllMatchingDataViewsXPaths(conn, initialXPaths, allXPaths);
+		
+		System.out.println("Finished getting the XPaths of all matching DataViews");
+		System.out.println("Proceeding with quering each one of them and writing to files");
+
+		/* 3. Iterate over that set of XPaths */
+		// execute a query with each of these XPaths which match **only 1 DataView at a time**
+		
+		final Iterator iterator = allXPaths.iterator();
+		
+		for(String xpath : allXPaths){
+		
+		final CountDownLatch latch = new CountDownLatch(1);
 			
+		DataViewQuery query = DataViewQuery.create((String) iterator.next());
+
+		Closable closable = conn.execute(query,
+				new Callback<DataViewChange>() {
+			@Override
+			public void callback(final DataViewChange change) {
+
+				latch.countDown();
+				/* 4. Write output to file */
+				System.out.println(change.getData().toString());
+				System.out.println("Data obtained at "+System.currentTimeMillis());
+
+				Writer writer = null;
+
+				try {
+					writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("outputFiles/"+((String) iterator.next()).replace('\\', '-').replace('/', '-').replace('"', ' '))));
+					writer.write(change.getData().toString());
+					writer.close();
+
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				} finally {
+					try {writer.close();} catch (Exception ex) {}
+				}
+			}
+		},
+		new ErrorCallback() {
+			@Override
+			public void error(final Exception exception) {
+				System.err.println("Error retrieving DataSet: " + exception);
+			}
+		}
+				);
+		
+        //close the query after the DataView has been queried and written to file
+		try {
+			latch.await(); 
+		} catch (InterruptedException e) {
+			System.err
+			.println("Interrupted exception while waiting the latch");
+		} finally {
+			//System.out.println("Finished writing "+xpath);
+			closable.close();
+			//System.out.println("Total number of DataViews' XPaths so far: " + allXPaths.size());
+		}
+
+		}
+
+	}
+
+	private static void getAllMatchingDataViewsXPaths(Connection conn,
+			ArrayList<String> initialXPaths, final Set<String> allXPaths) {
+		for(final String path : initialXPaths){
+						
 			CountDownLatch latch = new CountDownLatch(1);
 			
 			DataViewQuery query = DataViewQuery.create(path);
@@ -52,8 +130,6 @@ public final class SimpleBridgeMain {
 	                    		
 	                    	allXPaths.add(change.getSourceId());
 	                    		
-	                    	System.out.println(allXPaths.size() + path);
-	                    		
 	                    }
 	                },
 	                new ErrorCallback() {
@@ -64,65 +140,18 @@ public final class SimpleBridgeMain {
 	                }
 	        );
 	        
-	        /* how to ensure that the query is closed and no more updates
-	         * are received once we get all the matching DataViews? */
-	        
-	        //close the closable after e.g. 5 seconds?
+	        //close the query after a period of time
 			try {
-				boolean result = latch.await(5, TimeUnit.SECONDS); 
-				if (result) {
-					System.out.println("Latch triggered - should be impossible");
-				} else {
-					System.out.println("Latch timed out");
-				}
+				latch.await(5, TimeUnit.SECONDS); 
 			} catch (InterruptedException e) {
 				System.err
-				.println("Interrupted exception when waiting for POPULATED");
+				.println("Interrupted exception while waiting the latch");
 			} finally {
 				System.out.println("Finished with "+path);
-				closable.close(); 
+				closable.close();
+				System.out.println("Total number of DataViews' XPaths so far: " + allXPaths.size());
 			}
 	        
 		}
-		
-
-		/* 3. Iterate over that list */
-		// execute a query with each of these XPaths which match only 1 DataView at a time
-		
-//		DataViewQuery query = DataViewQuery.create(path);
-//
-//		conn.execute(query,
-//				new Callback<DataViewChange>() {
-//			@Override
-//			public void callback(final DataViewChange change) {
-//
-//				/* 4. Write output to file */
-//				System.out.println(change.getData().toString());
-//				System.out.println("Data obtained at "+System.currentTimeMillis());
-//
-//				Writer writer = null;
-//
-//				try {
-//					writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("outputFiles/"+path.replace('\\', '-').replace('/', '-').replace('"', ' '))));
-//					writer.write(change.getData().toString());
-//					writer.close();
-//
-//				} catch (IOException ex) {
-//					// report
-//				} finally {
-//					try {writer.close();} catch (Exception ex) {}
-//				}
-//			}
-//		},
-//		new ErrorCallback() {
-//			@Override
-//			public void error(final Exception exception) {
-//				System.err.println("Error retrieving DataSet: " + exception);
-//			}
-//		}
-//				);
-
-
-
 	}
 }
