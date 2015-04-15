c94e3073d25608189e991d7168bd34e5dc6accd3
Milka Horozova <mhorozova@ldn.itrs>
2015-04-15 13:23:00 +0100

    added ErrorCallback to the request method

--- src/SimpleBridgeMain.java
+++ src/SimpleBridgeMain.java
@@ -23,128 +23,154 @@ import com.itrsgroup.openaccess.dataview.DataViewTracker;
 
 public final class SimpleBridgeMain {
 
-    private final DataViewTracker tracker = new DataViewTracker();
-    private final CountDownLatch cdl = new CountDownLatch(1);
-    private DataView DataView;
-	
+	private final DataViewTracker tracker = new DataViewTracker();
+	private final CountDownLatch cdl = new CountDownLatch(1);
+	private DataView DataView;
+
 	public static void main(final String[] args) {
 
 		/* 1. Connect to cluster */
 		Connection conn = OpenAccess.connect("geneos.cluster://192.168.220.41:2551?username=admin&password=admin");
 
 		/* 2. Get a list of all DataViews */
-		
+
 		/*
 		 * The customer is testing it with 10 XPaths matching 4000 DataViews
 		 * but would like to subscribe to 50+ XPahts matching 20 000+ DataViews in production
 		 */
-		
-			/* customer defined list of 50+ XPaths that match 20 000+ DataViews */
-			ArrayList<String> initialXPaths = new ArrayList<String>();
-			
-			// test with a couple of XPaths
-			/* make sure an XPath doesn't match too many DataViews
-			 * as that may overload the node?*/
-			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-1\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-4\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-5\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-		//	initialXPaths.add("/geneos/gateway/directory/probe/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-			
-			/* all XPaths 20 000+, every XPath matches a single DataView */
-			final Set<String> allXPaths = new HashSet<String>();
-			
+
+		/* customer defined list of 50+ XPaths that match 20 000+ DataViews */
+		ArrayList<String> initialXPaths = new ArrayList<String>();
+
+		// test with a couple of XPaths
+		/* make sure an XPath doesn't match too many DataViews
+		 * as that may overload the node?*/
+		initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-25-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-4\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-1-probe-5\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-2-probe-1\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-2-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-2-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-2-probe-4\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-2-probe-5\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-3-probe-1\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-3-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-3-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-3-probe-4\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-3-probe-5\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-4-probe-1\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-4-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-4-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-4-probe-4\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-4-probe-5\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-5-probe-1\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-5-probe-2\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-5-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-5-probe-4\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//			initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-5-probe-5\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+		//initialXPaths.add("/geneos/gateway/directory/probe/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
+
+		/* all XPaths 20 000+, every XPath matches a single DataView */
+		final Set<String> allXPaths = new HashSet<String>();
+
 		/*
 		 * For each XPath, get a list of the XPaths of all matching DataViews (0 or many)
 		 */	
-		getAllMatchingDataViewsXPaths(conn, initialXPaths, allXPaths);
-		
+		getAllMatchingDataViewsXPaths(conn, initialXPaths, allXPaths, 5);
+
 		System.out.println("Finished getting the XPaths of all matching DataViews");
 		System.out.println("Proceeding with quering each one of them and writing to files");
 
 		/* 3. Iterate over that set of XPaths */
 		// execute a query with each of these XPaths which match **only 1 DataView at a time**
-		
-        iterateAndWrite(conn, allXPaths);
+
+		iterateAndWrite(conn, allXPaths);
 
 	}
 
 	private static void iterateAndWrite(Connection conn,
 			final Set<String> allXPaths) {
 		for(String s : allXPaths){
-        	
-        DataViewQuery query = DataViewQuery.create(s);
-        DataView DataView = new SimpleBridgeMain().request(conn, query, 5, SECONDS);
-        System.out.println(DataView);
-        Writer writer = null;
-        
-		try {
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("outputFiles/"+s.replace('\\', '-').replace('/', '-').replace('"', ' '))));
-			writer.write(DataView.toString());
-			writer.close();
+			DataViewQuery query = DataViewQuery.create(s);
+			DataView DataView = new SimpleBridgeMain().request(conn, query, 2, SECONDS);
 
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			try {writer.close();} catch (Exception ex) {}
+			Writer writer = null;
+
+			try {
+				writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("outputFiles/"+s.replace('\\', '-').replace('/', '-').replace('"', ' '))));
+				writer.write(DataView.toString());
+				writer.close();
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			} finally {
+				try {writer.close();} catch (Exception ex) {}
+			}
+
 		}
-        
-        }
 	}
-	
-    public DataView request(Connection conn, DataViewQuery query, long timeout, TimeUnit timeUnit) {
-        Closable c = conn.execute(query,
-                new Callback<DataViewChange>() {
-                    public void callback(final DataViewChange data) {
-                        DataView = tracker.update(data);
-                        //System.out.println(DataView);
-                        // TODO: Determine if we are 'done' here by examining DataView.
-                        // Once the condition is fulfilled, you can call cdl.countDown...
-                        cdl.countDown();
-                    }
-                }, null // Ignore errors, return null
-        );
 
-        try {
-            cdl.await(timeout, timeUnit);
-            c.close();
-            System.out.println("CLOSED query");
-        } catch (InterruptedException e) {
-            // Ignore errors, return null
+	public DataView request(Connection conn, DataViewQuery query, long timeout, TimeUnit timeUnit) {
+		Closable c = conn.execute(query,
+				new Callback<DataViewChange>() {
+			public void callback(final DataViewChange data) {
+				DataView = tracker.update(data);
+				//System.out.println(DataView);
+				cdl.countDown();
+			}
+		},                 new ErrorCallback() {
+            @Override
+            public void error(final Exception exception) {
+                System.err.println("Error retrieving DataView: " + exception);
+            }
         }
+				);
 
-        return DataView;
-    }
+		try {
+			cdl.await(timeout, timeUnit);
+			c.close();
+			// System.out.println("CLOSED query");
+		} catch (InterruptedException e) {
+			// Ignore errors, return null
+			System.out.println("Something's wrong... Interrupted while waiting for updates");
+			e.printStackTrace();
+		}
+
+		return DataView;
+	}
 
 	private static void getAllMatchingDataViewsXPaths(Connection conn,
-			ArrayList<String> initialXPaths, final Set<String> allXPaths) {
+			ArrayList<String> initialXPaths, final Set<String> allXPaths, int time) {
 		for(final String path : initialXPaths){
-						
+
 			CountDownLatch latch = new CountDownLatch(1);
-			
+
 			DataViewQuery query = DataViewQuery.create(path);
 
-	        Closable closable = conn.execute(query,
-	                new Callback<DataViewChange>() {
-	                    @Override
-	                    public void callback(final DataViewChange change) {	                    		
-	                    		
-	                    	allXPaths.add(change.getSourceId());
-	                    		
-	                    }
-	                },
-	                new ErrorCallback() {
-	                    @Override
-	                    public void error(final Exception exception) {
-	                        System.err.println("Error retrieving DataSet: " + exception);
-	                    }
-	                }
-	        );
-	        
-	        //close the query after a period of time
+			Closable closable = conn.execute(query,
+					new Callback<DataViewChange>() {
+				@Override
+				public void callback(final DataViewChange change) {	                    		
+
+					allXPaths.add(change.getSourceId());
+
+				}
+			},
+			new ErrorCallback() {
+				@Override
+				public void error(final Exception exception) {
+					System.err.println("Error retrieving DataSet: " + exception);
+				}
+			}
+					);
+
+			//close the query after a period of time,
+			// get all DataViews that have matched the query
+			// until this path expires
 			try {
-				latch.await(5, TimeUnit.SECONDS); 
+				latch.await(time, TimeUnit.SECONDS); 
 			} catch (InterruptedException e) {
 				System.err
 				.println("Interrupted exception while waiting the latch");
@@ -153,7 +179,7 @@ public final class SimpleBridgeMain {
 				closable.close();
 				System.out.println("Total number of DataViews' XPaths so far: " + allXPaths.size());
 			}
-	        
+
 		}
 	}
 }
