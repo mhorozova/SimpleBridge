b8a24bedac1ce574daf2234b3fea9190728bb5cc
Milka Horozova <mhorozova@ldn.itrs>
2015-05-20 10:44:07 +0100

    cleared merge conflicts

--- src/Main.java
+++ src/Main.java
@@ -1,141 +1,20 @@
-<<<<<<< HEAD
-import java.io.Writer;
-import java.util.ArrayList;
-=======
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
->>>>>>> Timer
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-<<<<<<< HEAD
-=======
 import org.apache.log4j.Logger;
 
->>>>>>> Timer
 import com.itrsgroup.openaccess.Callback;
 import com.itrsgroup.openaccess.Closable;
 import com.itrsgroup.openaccess.Connection;
 import com.itrsgroup.openaccess.ErrorCallback;
 import com.itrsgroup.openaccess.OpenAccess;
-<<<<<<< HEAD
-import com.itrsgroup.openaccess.dataview.DataView;
-import com.itrsgroup.openaccess.dataview.DataViewChange;
-import com.itrsgroup.openaccess.dataview.DataViewQuery;
-import com.itrsgroup.openaccess.dataview.DataViewTracker;
-
-public class Main {
-	
-	/* customer defined list of 50+ XPaths that match 20 000+ DataViews */
-	public static ArrayList<String> initialXPaths;
-	
-	/* all XPaths 20 000+, every XPath matches a single DataView */
-	public static Set<String> allXPaths;
-	
-	public static Connection conn;
-	
-	public static Writer writer;
-	
-	/* to be used in every new run */
-	public final static DataViewTracker tracker = new DataViewTracker();
-	public final static CountDownLatch cdl = new CountDownLatch(1);
-	public static DataView DataView;
-	
-	public static void main(String[] args) throws InterruptedException{
-		
-		writer = null;
-		
-		initialXPaths = new ArrayList<String>();
-		
-		/* make sure an XPath doesn't match too many DataViews
-		 * as that may overload the node?*/
-
-		//initialXPaths.add("/geneos/gateway/directory/probe/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-		initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-8-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-		initialXPaths.add("/geneos/gateway/directory/probe[(@name=\"gateway-6-probe-3\")]/managedEntity/sampler[(@type=\"\")]/dataview[(@name=\"UpdateTest\")]");
-
-		
-		/* 1. Connect to cluster */
-		conn = OpenAccess.connect("geneos.cluster://192.168.220.41:2551?username=admin&password=admin");
-		
-		/* 2. Get a list of all DataViews */
-
-		/*
-		 * The customer is testing it with 10 XPaths matching 4000 DataViews
-		 * but would like to subscribe to 50+ XPahts matching 20 000+ DataViews in production
-		 */
-		
-		allXPaths = new HashSet<String>();
-
-		getAllMatchingDataViewsXPaths(Main.conn, initialXPaths, allXPaths, 5);
-		
-		/* Iterate over the set of all XPaths, execute requests and write the results to files */
-		
-		Quartz.startScheduler();
-	}
-	
-	/**
-	 * Iterates over the customer-defined list of XPaths (expected 50+), each one of which
-	 * matches 0 or many DataViews
-	 * 
-	 * Executes a query with each XPath and adds all matching DataViews' XPaths
-	 * to the set of all XPaths
-	 * 
-	 * If the current query doesn't return anything for a certain amount of time
-	 * (e.g. 5 seconds), the count down latch times out
-	 * 
-	 * @param conn
-	 * @param initialXPaths
-	 * @param allXPaths
-	 * @param time
-	 */
-	static void getAllMatchingDataViewsXPaths(Connection conn,
-			ArrayList<String> initialXPaths, final Set<String> allXPaths, int time) {
-		for(final String path : initialXPaths){
-
-			CountDownLatch latch = new CountDownLatch(1);
-
-			DataViewQuery query = DataViewQuery.create(path);
-
-			Closable closable = conn.execute(query,
-					new Callback<DataViewChange>() {
-				@Override
-				public void callback(final DataViewChange change) {	                    		
-
-					allXPaths.add(change.getSourceId());
-
-				}
-			},
-			new ErrorCallback() {
-				@Override
-				public void error(final Exception exception) {
-					System.err.println("Error retrieving DataView: " + exception);
-				}
-			}
-					);
-
-			//close the query after a period of time,
-			// get all DataViews that have matched the query
-			// until this path expires
-			try {
-				latch.await(time, TimeUnit.SECONDS); 
-			} catch (InterruptedException e) {
-				System.err
-				.println("Interrupted exception while waiting the latch");
-			} finally {
-				System.out.println("Finished with "+path);
-				closable.close();
-				System.out.println("Total number of DataViews' XPaths so far: " + allXPaths.size());
-			}
-
-		}
-	}
-	
-=======
 import com.itrsgroup.openaccess.dataview.DataViewChange;
 import com.itrsgroup.openaccess.dataview.DataViewQuery;
 
@@ -279,5 +158,4 @@ public class Main {
 		return i;
 	}
 
->>>>>>> Timer
 }
